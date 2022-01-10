5f52f84d5836799dc95494e9bc4d5ced6c6b885f
Pritam Kar <pritamkar13@gmail.com>
2022-01-10 14:15:49 +0530

    server:createPost controller added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,6 +10,15 @@ export const getPosts = async(req, res) =>{
     }
 }
 
-export const createPost = (req, res) =>{
-    res.send('Post Works');
+export const createPost = async(req, res) =>{
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+        res.status(201).json(newPostMessage);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
