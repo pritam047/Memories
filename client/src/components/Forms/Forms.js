36b89773f618e12280d00885d0b999058781da88
Pritam Kar <pritamkar13@gmail.com>
2022-01-18 12:31:01 +0530

    new components added - Posts,Post and Forms.js

---
+++ client/src/components/Forms/Forms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            Forms
+        </div>
+    )
+}
+
+export default Form;
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div>
+            single Post
+        </div>
+    )
+}
+
+export default Post
