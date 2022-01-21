b037131a956170bd5189e0d20b92163de378883f
Pritam Kar <pritamkar13@gmail.com>
2022-01-21 21:28:15 +0530

    Posts styles added

---
+++ client/src/components/Posts/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+  container: {
+    width: '100%', 
+    margin: 0,
+  },
+}));
