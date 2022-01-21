21b464a5dca4134b106bd0a0b46e87923e02d3da
Pritam Kar <pritamkar13@gmail.com>
2022-01-21 20:57:13 +0530

    actions updated

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts; // to be modified
+            return action.payload; // to be modified
         case 'CREATE':
-            return posts; // to be modified
+            return [...posts, action.payload]; // to be modified
         default:
             return posts;    
     }
