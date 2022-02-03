e5b437aeefe0263268fe4cf46bfac657cc4c40a9
Pritam Kar <pritamkar13@gmail.com>
2022-02-03 21:03:11 +0530

    Individual Post component done

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import moment from 'moment'
-import useStyles from './styles';
+import useStyles from './styles'
 
 const Post = ({post}) => {
     const classes = useStyles();
@@ -18,7 +18,7 @@ const Post = ({post}) => {
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
         <div className={classes.overlay2}>
-          <Button style={{ color: 'white' }} size="small" ><MoreHorizIcon fontSize="default" /></Button>
+          <Button style={{ color: 'white' }} size="small" ><MoreHorizIcon fontSize="medium" /></Button>
         </div>
         <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
