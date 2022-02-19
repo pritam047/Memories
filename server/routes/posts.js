7ce9cd39008bec37329f9b2ad06c4cdc7c5db1f8
Pritam Kar <pritamkar13@gmail.com>
2022-02-20 01:24:59 +0530

    auth middleware added to routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,8 +3,8 @@ import { createPost, getPosts, updatePost, deletePost, likePost } from "../contr
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 export default router;
