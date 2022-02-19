a15bc20aebc4ca3a241bcc7fc680e90cdd1b1e46
Pritam Kar <pritamkar13@gmail.com>
2022-02-19 14:27:40 +0530

    google oauth login and logout done

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import  {AppBar, Toolbar, Typography, Avatar, Button} from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
+
+import { LOGOUT } from '../../constants/actionTypes';
+
 const Navbar = () => {
     const classes = useStyles();
-    const user = null;
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
+    const logout = () =>{
+      dispatch({ type: LOGOUT });
+      navigate('/');
+      setUser(null);
+    }
+
+    useEffect(() =>{
+      setUser(JSON.parse(localStorage.getItem('profile')));
+    },[location])
+
     return (
     <AppBar className={classes.appBar} position="static" color="inherit">
     <div className={classes.brandContainer}>
@@ -18,7 +36,7 @@ const Navbar = () => {
             <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={()=>console.log('Bye')}>Logout</Button>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ): (
             <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
