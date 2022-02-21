769cc7e3f01a9792a67d1c89761addcbeaef4aa2
Pritam Kar <pritamkar13@gmail.com>
2022-02-21 12:05:57 +0530

    likePost functionality updated for auth users

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -53,11 +53,21 @@ export const deletePost = async(req, res) =>{
 
 export const likePost = async(req,res) => {
     const {id} = req.params;
-
+    
+    if (!req.userId) {
+        return res.json({ message: "Unauthenticated" });
+      }
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`); // check if valid id exists
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new: true});
+    const index = post.likes.findIndex((id) => id ===String(req.userId));
+
+    if (index === -1) {
+      post.likes.push(req.userId);  // like the post
+    } else {
+      post.likes = post.likes.filter((id) => id !== String(req.userId)); // else, dislike the post
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {new: true});
 
     res.json(updatedPost);
 }
