bbb8507a9baa0387ca561c9f70745643e1e43dc7
Pritam Kar <pritamkar13@gmail.com>
2022-02-21 13:16:14 +0530

    Creator input field removed and set by default for logged in user

--- client/src/components/Forms/Forms.js
+++ client/src/components/Forms/Forms.js
@@ -9,38 +9,46 @@ import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({currentId, setCurrentId}) => {
 
-    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    const [postData, setPostData] = useState({ title: '', message: '', tags: '', selectedFile: '' });
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     
     const classes = useStyles();
     const dispatch = useDispatch();
     
+    const user = JSON.parse(localStorage.getItem('profile'))
     useEffect(() =>{
         if(post) setPostData(post);
     }, [post])
     const clear = () => {
         setCurrentId(0);
-        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+        setPostData({ title: '', message: '', tags: '', selectedFile: '' });
       };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         if(currentId){
-            dispatch(updatePost(currentId, postData));
+            dispatch(updatePost(currentId, {...postData, name: user?.result?.name}));
         }else{
-            dispatch(createPost(postData));
+            dispatch(createPost({...postData, name: user?.result?.name}));
         }
         clear();
     }
 
+    if (!user?.result?.name) {
+        return (
+          <Paper className={classes.paper}>
+            <Typography variant="h6" align="center">
+              Please Sign In to create your own memories and like other's memories.
+            </Typography>
+          </Paper>
+        );
+      }
+      
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth
-                    value={postData.creator}
-                    onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth
                     value={postData.title}
                     onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
