16142dc4ac3a79b1a8a2020ea6fcb652250bdeb2
Pritam Kar <pritamkar13@gmail.com>
2022-02-21 14:17:52 +0530

    edit func. modified for auth users

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -37,10 +37,13 @@ const Post = ({post, setCurrentId}) => {
           <Typography variant="h6">{post.name}</Typography>
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && 
+        (
         <div className={classes.overlay2}>
           <Button style={{ color: 'white' }} size="small" 
           onClick={()=> setCurrentId(post._id)}><MoreHorizIcon fontSize="medium" /></Button>
         </div>
+        )}
         <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
         </div>
