903dfd08fed7b3a1ad48287d439a8b9a196c7de0
Pritam Kar <pritamkar13@gmail.com>
2022-02-21 12:07:32 +0530

    signin & signup api index added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,8 +8,8 @@ export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updated
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
-export const signIn = (formData) => API.post('/user/signin');
-export const signUp = (formData) => API.post('/user/signup');
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
 
 
 
